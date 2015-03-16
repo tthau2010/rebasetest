84fd74df0b67022e898a6bd7e4a85c54503e342d
unknown <tthau2010@gmail.com>
2015-03-16 21:54:23 +0800

    rebase day 2

---
+++ test.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+class Car
+{
+    var $color;
+    function Car($color="green") {
+      $this->color2 = $color;
+    }
+}
+
+function print_vars($obj) {
+   foreach (get_object_vars($obj) as $prop => $val) {
+     echo "\t$prop = $val\n";
+   }
+}
+
+// instantiate one object
+$herbie = new Car("white");
+
+// show herbie properties
+echo "\herbie: Properties\n";
+print_vars($herbie);
+
+?>  
+
+</body>
+</html>
